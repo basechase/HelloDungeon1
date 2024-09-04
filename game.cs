b8a40983abc632c5e96e6945d7963bac6a0662c9
basechase <chase7y@gmail.com>
2024-09-04 16:57:12 -0500

    added function overflow for option3 function

--- game.cs
+++ game.cs
@@ -10,64 +10,82 @@ namespace hello_dungeon
 {
     internal class Game
     {
+        struct Player
+        {
+            public float playerHealth;
+            public float playerMana;
+            public float playerGold;
+            public string playerRole;
+            public int playerPotion;
+
+            public Player( 
+               float playerHealth,
+               float playerMana,
+               float playerGold,
+               string playerRole,
+               int playerPotion)
+            {
+                this.playerHealth = playerHealth;
+                this.playerMana = playerMana;
+                this.playerGold = playerGold;
+                this.playerRole = playerRole;
+                this.playerPotion = playerPotion;
+            }
+        }
+
+
+        static void PrintPlayerStats(Player player)
+        {
+            Console.WriteLine(player.playerRole + " Health: " + player.playerHealth);
+            Console.WriteLine(player.playerRole + " Mana:   " + player.playerMana);
+            Console.WriteLine(player.playerRole + " Gold:   " + player.playerGold);
+            Console.WriteLine("Role Selected:   " + player.playerRole);
+            Console.WriteLine(player.playerRole + " Health Potions:   " + player.playerPotion);
+
+        }
+
+
+
+
+
+
         public void Run()
         {
-            string playerName = "BobLee";
-            float playerHealth = 10.0f;
-            float playerMana = 5.0f;
-            int playerGold = 3;
-            bool playerTorch = true;
-            string playerRole = "Fighter";
 
-            Console.WriteLine("Hello " + playerName);
-            Console.WriteLine();
-            Console.WriteLine("Welcome to my dungeon");
-            Console.WriteLine();
-            Console.WriteLine("Health " + playerHealth);
-            Console.WriteLine();
-            Console.WriteLine("Mana " + playerMana);
-            Console.WriteLine();
-            Console.WriteLine("Gold " + playerGold);
-            Console.WriteLine();
-            int input = GetInput("Are you a ", "Warrior", " Fighter", " Wizard");
+            Player player1 = new Player(playerHealth: 100, playerMana: 100, playerGold: 100, playerRole: "", playerPotion: 1);
+
+
+            
+            int input = GetInput3("Are you a ", "Warrior", " Fighter", " Wizard");
 
 
             if (input == 1)
             {
-                playerRole = "Warrior";
+                player1.playerRole = "Warrior";
             }
             else if (input == 2)
             {
-                playerRole = "Fighter";
+                player1.playerRole = "Fighter";
+            }
+            else if (input == 3)
+            {
+                player1.playerRole = "Wizard";
             }
             else
             {
-                playerRole = "Wizard";
+                Console.WriteLine("Invalid Selection");
             }
 
+            Console.WriteLine();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            Console.WriteLine("Player role: " + playerRole + " selected" + playerName);
-
+            PrintPlayerStats(player1);
 
             Console.WriteLine();
 
-            Console.WriteLine();
+            // function GetInput2 used, only takes two options/strings after the initial question
 
-            input = GetInput("You wake up in a dark cave, out of the shadows a car salesman appears regarding you cars extended warranty. " +
-                "Do you pass on or conversate?", " Pass on", " Talk", ">");
+
+            input = GetInput2("You wake up in a dark cave, out of the shadows a car salesman appears regarding you cars extended warranty. Do you pass on or conversate?", "Pass on", "Conversate");
             if (input == 1)
             {
                 Console.WriteLine("Care salesman: Well fairwell then!");
@@ -79,142 +97,203 @@ namespace hello_dungeon
             Console.WriteLine();
 
 
+            input = GetInput2("You step onto an algae covered rock and slip into a hole, do you use your special ability or go to sleep?",
+                "1: Sleep", "2: Use ability");
 
-
-
-
-            input = GetInput("You step onto an algae covered rock and slip into a hole, do you use your special ability or go to sleep?",
-                "1: Sleep", "2: Use ability", ">");
-
-
-
-
-
-
+            
             Console.WriteLine();
 
 
             if (input == 1)
             {
                 Console.WriteLine("You goto sleep, upon awakening the hole is suddenly flooded enough for you to escape.");
-                if (playerRole == "Wizard")
+                if (player1.playerRole == "Wizard")
                 {
                     Console.WriteLine();
                     Console.WriteLine("You cast a short lasting fire spell, drying yourself off.");
+                    SubMana();
                 }
 
             }
-            else if (input == 2 && playerRole == "Warrior")
+            else if (input == 2 && player1.playerRole == "Warrior")
             {
 
                 Console.WriteLine("You charge up and leap out of the deep hole in a single bound.");
+                SubMana();
             }
-            else if (input == 2 && playerRole == "Wizard")
+            else if (input == 2 && player1.playerRole == "Wizard")
             {
 
                 Console.WriteLine("You cast a levatation spell and float out gracefully.");
+                SubMana();
             }
-            else if (input == 2 && playerRole == "Fighter")
+            else if (input == 2 && player1.playerRole == "Fighter")
             {
 
                 Console.WriteLine("You charge your fists and punch your way out the hole without a scratch.");
-
+                SubMana();
             }
 
 
 
-            Console.WriteLine();
-            input = GetInput("You notice a skeleton approaching you, do you flee or fight?", 
-                "1: Flee", "2: Fight", ">");
-            Console.WriteLine();
-            // you stopped here, change flightorflee to input
-            string fightOrFlee = "";
-            while (fightOrFlee != "1" && fightOrFlee != "2")
+            input = GetInput2("A coyote appears from the shadows, do you:", "Flee", "Fight");
+
+            // determine if the player wants to flee or fight, conditions based off of  playerRole 
+
+            if (input == 1 && player1.playerRole =="Warrior") 
             {
-                fightOrFlee = Console.ReadLine();
-                if (fightOrFlee == "1" && playerRole == "Wizard")
-                {
-
-                    Console.WriteLine("You teleport past the skeleton, he gets startled, falls over and dies.");
-                }
-                else if (fightOrFlee == "1" && playerRole == "Fighter")
-                {
-                    Console.WriteLine("You swiftly sprint, creating a small gust of wind that knocks the skeleton" +
-                        "into a pile of bone dust.");
-                }
-                else if (fightOrFlee == "1" && playerRole == "Warrior")
-                {
-                    Console.WriteLine("You sidestep past the skeleton, your souless eyes cause him to tremble," +
-                        "breaking all of his bones.");
-                }
-
-                if (fightOrFlee == "2" && playerRole == "Wizard")
-                {
-                    Console.WriteLine("You cast an ice spell, creating a skeleton popsicle.");
-                }
-                else if (fightOrFlee == "2" && playerRole == "Fighter")
-                {
-                    Console.WriteLine("You two piece the skeleton, breaking his ego and dome.");
-                }
-                else if (fightOrFlee == "2" && playerRole == "Warrior")
-                {
-                    Console.WriteLine("You grab the skeleton by the neck and RKO him into the hole.");
-                }
+                Console.WriteLine("You jump over the coyote, scaring he/she/they/them out of the cave.");
+            }
+            else if (input == 1 && player1.playerRole == "Wizard")
+            {
+                Console.WriteLine("You sprint away from the coyote, it dashes towards you and slips into the hole. You walk on.");
+            }
+            else if (input == 1 && player1.playerRole == "Fighter")
+            {
+                Console.WriteLine("The coyote charges, you sidestep and it knocks itself out on the cave wall.");
+            }
+            else if (input == 2 && player1.playerRole == "Warrior")
+            {
+                Console.WriteLine("You curb stomp the coyote into a puddle of mush.");
+                SubMana();
+            }
+            else if (input == 2 && player1.playerRole == "Wizard")
+            {
+                Console.WriteLine("You cast an ice spell, freezing the coyote solid.");
+                SubMana();
+            }
+            else if (input == 2 && player1.playerRole == "Fighter")
+            {
+                Console.WriteLine("You two piece the coyote caving in his dome.");
+                SubMana();
             }
 
+           
 
 
+                //SubMana function for special ability use, prints playerstats afterwards
+                int SubMana()
+                
+                {
+                    Console.WriteLine();
+                    player1.playerMana -= 20;
+                    PrintPlayerStats(player1);
+                    Console.WriteLine();
+                    return 0;
+                }
+            
+                    
+                
 
-
-
-        }
-
-
-        int GetInput(string description, string option1, string option2, string option3)
-        {
-            string input = "";
-            int inputRecieved = 0;
-            // Input Loop
-            while (inputRecieved != 1 && inputRecieved != 2 && inputRecieved != 3)
+            //work in progress mana/health potion
+            int Potion()
             {
-                //Print Options
-                Console.WriteLine(description);
-                Console.WriteLine("1. " + option1);
-                Console.WriteLine("2. " + option2);
-                Console.WriteLine("3. " + option3);
-
-                //Get input from player
-
-                input = Console.ReadLine();
-
-                //IF player slected first option
-                if (input == "1" || input == option1)
+                if (player1.playerHealth <= 80)
                 {
-                    inputRecieved = 1;
+                    Console.WriteLine();
+                    player1.playerPotion -= 1;
+                    player1.playerHealth += 20;
+                    Console.WriteLine("Your health is now: " + player1.playerHealth);
+                    Console.WriteLine();
                 }
-                //otherwise if second option selected
-                else if (input == "2" || input == option2)
-                {
-                    //set input recieved to the second option
-                    inputRecieved = 2;
-
-                }
-                else if (input == "3" || input == option3)
-                {
-                    //set input rcvd to the third option
-                    inputRecieved = 3;
-                }
-                //if neither true
                 else
                 {
-                    Console.WriteLine("Invalid Input");
-                    Console.ReadKey();
+                    Console.WriteLine("Your health isn't low enough!");
+
                 }
+                return 0;
             }
 
-            Console.Clear();
-            return inputRecieved;
+
+
+
+            // GetInput2 
+            int GetInput2(string description, string option1, string option2)
+            {
+                string input = "";
+                int inputRecieved = 0;
+                while (inputRecieved != 1 && inputRecieved != 2)
+                {
+                    Console.WriteLine(description);
+                    Console.WriteLine("1. " + option1);
+                    Console.WriteLine("2. " + option2);
+                    Console.WriteLine(">. ");
+
+                    input = Console.ReadLine();
+
+                    if (input == "1" || input == option1)
+                    {
+                        inputRecieved = 1;
+                    }
+                    else if (input == "2" || input == option2)
+                    {
+                        inputRecieved = 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input goober");
+                    }
+                }
+                Console.Clear();
+                return inputRecieved;
+
+
+            }
+
+
+
+
+
+
+            int GetInput3(string description, string option1, string option2, string option3)
+            {
+                string input = "";
+                int inputRecieved = 0;
+                // Input Loop
+                while (inputRecieved != 1 && inputRecieved != 2 && inputRecieved != 3)
+                {
+                    //Print Options
+                    Console.WriteLine(description);
+                    Console.WriteLine("1. " + option1);
+                    Console.WriteLine("2. " + option2);
+                    Console.WriteLine("3. " + option3);
+                    Console.WriteLine(">.");
+
+                    //Get input from player
+
+                    input = Console.ReadLine();
+
+                    //IF player slected first option
+                    if (input == "1" || input == option1)
+                    {
+                        inputRecieved = 1;
+                    }
+                    //otherwise if second option selected
+                    else if (input == "2" || input == option2)
+                    {
+                        //set input recieved to the second option
+                        inputRecieved = 2;
+
+                    }
+                    else if (input == "3" || input == option3)
+                    {
+                        //set input rcvd to the third option
+                        inputRecieved = 3;
+                    }
+                    //if neither true
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey();
+                    }
+
+                }
+
+                Console.Clear();
+                return inputRecieved;
+            }
         }
+
     }
 }
 
